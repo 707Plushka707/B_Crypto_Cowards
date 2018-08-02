600c59177842f979d985b7a62e7b697533a2f218
bicced <jefftseng1994@gmail.com>
2018-08-02 16:40:43 -0400

    toec2

--- auth/originCheck.js
+++ auth/originCheck.js
@@ -3,7 +3,7 @@
 exports.originCheck = function(req, res, next){
  const origin = req.get('origin')
  if (process.env.NODE_ENV === 'production') {
-   if (origin.indexOf('renthero.com') > -1) {
+   if (origin.indexOf('cryptocowards.com') > -1) {
      next()
    } else {
      res.status(500).send({
